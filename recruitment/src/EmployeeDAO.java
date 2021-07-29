fb5948176ecf16fc178870782fb4b44d722925db
AustinTerranova <35577443+AustinTerranova@users.noreply.github.com>
2021-07-29 13:11:57 -0500

    Revert "Merge pull request #3 from TechForGoodInc/specificRecruitmentFiles"
    This reverts commit c65dd208575e415153b177e0a8965bebdd60e495, reversing
    changes made to 2929a075398c699c3d56af89e0ddabbb2e160d5c.

--- recruitment/src/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.Vector;
-
-public interface EmployeeDAO {
-	
-	public Employee getEmployeeByCode(String code);
-	public Vector<Employee> getAllEmployees();
-	public void addEmployee(Employee employee);
-
-}
